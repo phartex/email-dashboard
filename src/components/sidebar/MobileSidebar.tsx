dd48efb83307a8854d02d6b0e5383771456cb1b6
victor fateru <victor.fateru@interswitchng.com>
2025-10-04 16:03:00 +0100

    update

--- src/components/sidebar/MobileSidebar.tsx
+++ src/components/sidebar/MobileSidebar.tsx
@@ -2,20 +2,36 @@
 
 
 import { useUIStore } from "@/store/ui";
-import { CircleX } from "lucide-react";
+import { CircleX, PowerIcon } from "lucide-react";
 import Image from "next/image";
 import React, {  useRef } from "react";
 import { Menu } from "./menu/Menu";
 import { useOnClickOutside } from "../hooks/useOutsideClick";
+import { useAuthStore } from "@/store/auth";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 const MobileSidebar: React.FC = () => {
   const { toggleSidebar } = useUIStore();
+
+    const { clearAuthUser } = useAuthStore((state) => state); 
+    const router = useRouter();
 
   const handleToggleClick = () => {
     toggleSidebar();
   };
   const ref = useRef<HTMLDivElement | null>(null);
   useOnClickOutside(ref, () => handleToggleClick());
+    const handleLogout = () => {
+    // Clear Zustand state
+    clearAuthUser();
+
+    // Remove auth cookie
+    Cookies.remove("authUser");
+
+    // Redirect to login
+    router.push("/login"); // <-- Better to send them to /login, not just "/"
+  };
 
   return (
     <div
@@ -35,12 +51,13 @@ const MobileSidebar: React.FC = () => {
       <Menu />
 
       <div className="flex h-full flex-col">
-        {/* <div
-          className={`mt-auto flex h-[2.5rem] mx-auto cursor-pointer items-center gap-[0.875rem] pl-[0.875rem] text-xs font-normal hover:font-bold hover:text-primary-default`}
-          onClick={() => router.push("/")}
+        <div
+          className={`mx-auto mb-12 mt-auto flex h-[2.5rem] w-[223px] py-5 justify-center items-center gap-3 rounded-md border border-green-700 px-3 text-sm font-semibold hover:bg-red-50 hover:font-bold transition`}
+          onClick={handleLogout}
         >
-          Log Out
-        </div> */}
+        <PowerIcon className="h-6 w-6 -rotate-90 text-green-700" strokeWidth={2.5} />
+          <span className="text-green-700">Log Out</span>
+        </div>
       </div>
     </div>
   );
